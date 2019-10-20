551fccb9a2ad9fa97073e484f1b89b4315a0da5c
eternal golden bread <codylewisbrock@gmail.com>
2019-10-19 17:22:24 -0700

    adds the randomizer for reordering the overall questions as well as the choices within the questions.  also adds dom manipulation to get it into the screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,18 +17,53 @@ let questions = {
         ['Blink-182', 'Third Eye Blind', 'Red Hot Chili Peppers'],
         './assets/images/porkins.jpg'
     ),
-    questionThree : new TriviaCards
+    questionThree : new TriviaCards (
+        'Who wrote "something"',
+        'someone',
+        ['one', 'two', 'three'],
+        './assets/images/willrow-hood.jpg'
+    )
+}
+
+
+//creates a randomizer to use in the future
+function randomize (value) {
+    return (Math.floor(Math.random() * value));
 }
 
 //FUNCTIONS
+//initialize is run for each time we have a new question to show
 function initialize () {
 
-    let keyArr = Object.keys(questions);
-    console.log(keyArr);
+    //
+    let objArr = Object.values(questions);
+    console.log(objArr);
 
-    let randIndex = Math.floor(Math.random() * (keyArr.length));
-    console.log(randIndex);
+    let randIndex = randomize(objArr.length);
+    let [question, correct, incorrect, img] = Object.values(objArr[randIndex]);
+    console.log(objArr[randIndex]);
+
     
+    incorrect.push(correct);
+    
+    for (let i = 0; i < 3; i++) {
+        if (randomize(100) > 50) {
+            let temp = incorrect[i];
+            incorrect[i] = incorrect[i+1];
+            incorrect[i+1] = temp;
+        }
+    }
+
+    console.log(incorrect);
+
+    let html = `<ul>`;
+    incorrect.map(function(choice) {
+        html += `<li>${choice}</li>`
+    });
+    
+    $("#question").html(question);
+    $("#multiple-choice").html(html + "</ul>");
+
 }
 
 //CLICK HANDLERS
